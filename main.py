5184ff9cfce168b330f17d96f36a052373b44376
E11ie <jessieadrian3@gmail.com>
2018-09-20 11:34:23 -0500

    processing the form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from caesar import rotate_string
 
 app = Flask(__name__)
@@ -39,5 +39,12 @@ form = """
 @app.route("/")
 def index():
 	return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+	rot = request.form["rot"]
+	text = request.form["text"]
+
+	return "<h1>" + rotate_string(text, rot) + "</h1>"
 
 app.run()
